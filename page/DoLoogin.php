fca045bb40801b681ec49a1633facb1495ac26d7
Clément GOMEZ <clement.gomez@lycee-elie-vinet.org>
2023-04-14 15:51:11 +0200

    pb

--- page/DoLoogin.php
+++ page/DoLoogin.php
@@ -1,26 +1,44 @@
 <?php
 
+function debug_to_console($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
 
 $mail = isset ( $_POST ['Username'] ) ? $_POST ['Username'] : NULL;
 $pass = isset ( $_POST ['Password'] ) ? $_POST ['Password'] : NULL;
 
 if($mail == NULL || $pass == NULL){
-    header("Location: login.php");
-    die();
+    echo <<<EOF
+        <div>No mail or passWord</div>
+    EOF;
 }
 
 require_once ('../mysql/ControleurConnexion.php');
 $con = new ControleurConnexion();
 
-$datas = $con->consulter('*', 'userInfos', '', 'Mail='+$mail+'Mdp='+$pass, '', '', '', '');
+$where = 'Mail ="' . $mail  . '" & Mdp= "' . $pass . '"';
+$datas = $con->consulter('*', 'userInfos', '', $where , '', '', '', '');
 
-if($datas != NULL){
+if($datas[0] != NULL){
     $_SESSION["Id"] = $datas[0] ;
-    header("modifUserData.php");
-    die();
+    echo <<<EOF
+        <div>win</div>
+    EOF;
+    //header("Location : modifUserData.php");
+    debug_to_console('Sucess ! ID = ' . $datas[0] );
 }
 else{
-    header("Location: login.php");
+    debug_to_console("FAIL 2");
+    echo $mail;
+    echo <<<EOF
+        <div>lose</div>
+    EOF;
+    echo $pass;
+    //header("Location: login.php");
     die();
 }
 
