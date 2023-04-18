558ddec794aa4e77a03d2d637a028b8129036926
LesaintGuz <ulyssehave@gmail.com>
2023-04-18 16:34:19 +0200

    add forgotten file

---
+++ page/accountDeleteForm.php
@@ -0,0 +1,18 @@
+<?php
+session_abort();
+echo <<<EOF
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="utf-8" />
+<title>Paramètres</title>
+</head>
+<body>
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Procyon_lotor_qtl2.jpg/800px-Procyon_lotor_qtl2.jpg">
+<div>Clément GOMEZ & Ulysse HAV֤E</div>
+<div>Salut mon pote !</div>
+<div>
+<a href="login.php">Menu principal</a>
+    </div>
+    </body>
+EOF;
