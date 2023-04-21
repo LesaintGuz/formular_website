2625ea3d930225649bcf360e7da7be5a17211abd
Clément GOMEZ <clement.gomez@lycee-elie-vinet.org>
2023-04-21 15:36:01 +0200

    Backup sytem with powerShell script

---
+++ LaunchSaveTask.ps1
@@ -0,0 +1,18 @@
+﻿# POWER BY GPT v4
+
+# Importez le module TaskScheduler
+#Install-Module TaskScheduler
+
+# Définissez le chemin d'accès complet de votre script PowerShell
+$scriptPath = "C:\xampp\htdocs\SaveAutoBDD.ps1"
+
+# Définissez le déclencheur de la tâche pour qu'elle s'exécute toutes les heures
+$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddHours(20) -RepetitionInterval (New-TimeSpan -Hour 20) -RepetitionDuration (New-TimeSpan -Hours 23 -Minutes 59 -Seconds 59)
+
+#Supprimer la tâche si elle existe 
+Unregister-ScheduledTask -TaskName "SaveTask" -Confirm:$false
+
+
+# Créez la tâche planifiée
+Register-ScheduledTask -TaskName "SaveTask" -Trigger $trigger -Action $action -RunLevel Highest
+
---
+++ SaveAutoBDD.ps1
@@ -0,0 +1,25 @@
+﻿#What do you want to backup?
+$sourceFolder = "C:\xampp\mysql\data"
+#Where do you want to backup to?
+$destinationFolder = "C:\xampp\htdocs\BDDBackUp"
+
+#Figure out a destination folder with a timestamp.  Store it in a variable so that the same folder name is used throughout the script if it takes over a second to run
+$destinationFolderWithTimestamp = ($destinationFolder + "\" + (Get-Date -Format yyyy-mm-dd_HH-mmmm-ss))
+
+#if <destination folder>\<current date and time> doesn't exist - create it!
+If (!(Test-Path -Path $destinationFolderWithTimestamp)) {
+    New-Item -ItemType Directory -Path $destinationFolderWithTimestamp
+    Write-Host ($destinationFolderWithTimestamp + " Created")
+}
+
+try {
+    #try a backup - stop if it fails
+    Copy-Item -Recurse -Path $sourceFolder -Destination $destinationFolderWithTimestamp -ErrorAction Stop -force
+    #confirm backup worked
+    Write-Host "Backup Completed OK"
+    #launch an app - in this case notepad - the "&" needs to be kept as it denotes launching something
+    #& C:\Windows\notepad.exe
+} catch {
+    #Error happened - inform user and do not launch
+    Write-Host "Backup Failed - not launching app"
+}
